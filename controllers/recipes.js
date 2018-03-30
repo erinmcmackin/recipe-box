2f945e7baceeb5ed5e44a54764d6806306c7d54b
Trenton Marks <trentmarks111@gmail.com>
2018-03-30 12:06:30 -0600

    All routes created; index/get, create/post, destroy/delete, update/put; controllers/recipes.js complete

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,9 +4,30 @@ const Recipe = require('../models/recipes.js');
 
 // Index Route
 router.get('/', (req, res)=>{
-    res.send('Sending...')
+    Recipes.find({}, (err, foundRecipes)=>{
+        res.json(foundRecipes);
+    });
 });
 
+// Create Route
+router.post('/', (req, res)=>{
+    Recipes.create(req.body, (err, createdRecipe)=>{
+        res.json(createdRecipe);
+    });
+});
 
+// Delete Route
+router.delete('/:id', (req, res)=>{
+    Recipes.findByIdAndRemove(req.params.id, (err, deletedRecipe)=>{
+        res.json(deletedRecipe);
+    });
+});
+
+// Update Route
+router.put('/:id', (req, res)=>{
+    Recipes.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedRecipe)=>{
+        res.json(updatedRecipe);
+    });
+});
 
 module.exports = router;
